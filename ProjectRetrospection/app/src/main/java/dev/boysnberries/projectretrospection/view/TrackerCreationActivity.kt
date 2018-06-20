082033f4682ff2ff535d79a1e8a3f0ff1f330d8a
John Patterson <john@JohnPPatterson.com>
2018-06-19 19:59:44 -0700

    adds link between question list and question edit

--- ProjectRetrospection/app/src/main/java/dev/boysnberries/projectretrospection/view/TrackerCreationActivity.kt
+++ ProjectRetrospection/app/src/main/java/dev/boysnberries/projectretrospection/view/TrackerCreationActivity.kt
@@ -1,6 +1,7 @@
 package dev.boysnberries.projectretrospection.view
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -12,6 +13,7 @@ import dev.boysnberries.projectretrospection.data.samples.getQuestions
 // TODO: This is also copy & paste programming
 // It should also be abstracted. The notion should be something like a card view.
 class TrackerCreationActivity : Activity() {
+    public val OPEN_QUESTION_KEY: String = "OPEN_QUESTION"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +22,22 @@ class TrackerCreationActivity : Activity() {
         val questions = this.loadQuestions()
         val questionsView = findViewById<RecyclerView>(R.id.recycler_questions)
         questionsView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-        questionsView.adapter = QuestionAdapter(questions = questions)
+        questionsView.adapter = QuestionAdapter(
+                questions = questions,
+                onClickCallback = {
+                    val idx = questionsView.indexOfChild(it)
+                    this.openQuestionEditor(questions[idx])
+                }
+        )
     }
 
     private fun loadQuestions(): List<Question> {
         return getQuestions()
+    }
+
+    private fun openQuestionEditor(question: Question) {
+        val intent = Intent(this, QuestionCreationActivity::class.java)
+        intent.putExtra(OPEN_QUESTION_KEY, question)
+        this.startActivity(intent)
     }
 }
