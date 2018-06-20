a23f9145aa3c33ffb42fe9566d29c69ab6283842
Taylor Lanclos <tlanclos@live.com>
2018-06-19 23:38:58 -0500

    Remove tracker dao that is unnecessary

--- ProjectRetrospection/app/src/main/java/dev/boysnberries/projectretrospection/data/database/Database.kt
+++ ProjectRetrospection/app/src/main/java/dev/boysnberries/projectretrospection/data/database/Database.kt
@@ -5,16 +5,6 @@ import android.content.Context
 import dev.boysnberries.projectretrospection.data.database.entity.*
 
 
-@Dao
-interface TrackersDao {
-    @Query("select * from trackers")
-    fun getAll(): List<Tracker>
-
-    @Query("select * from questions where tracker = :trackerID")
-    fun getQuestionsForTrackerByID(trackerID: Long)
-}
-
-
 @Database(version = 1, entities = [
     BooleanAnswer::class,
     CategoricalAnswer::class,
